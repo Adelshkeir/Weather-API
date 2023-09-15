9f7ac7fb2ae7a49b10b6fbb98101031f86c0fafe
Adelshkeir <adelshkeir12@gmail.com>
2023-09-14 20:32:06 -0930

    Weather API: Extracting the values from the Fake Weather json

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React, { Component } from "react";
 import Search from "./components/Search";
 import WeatherItem from "./components/WeatherItem";
-
 import SayHi, { SayHello } from "./components/WeatherItem";
 import fakeWeatherData from "./fakeWeatherData.json";
-
 import "./App.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "Adel"
-    };
-  }
-
-  handleInputChange = value => {
-    this.setState({ name: value });
-  };
-
-  render() {
-    return (
-      <div className="app">
-         <Search/> 
-         <WeatherItem/> 
-
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app">
+      <Search  searchData={fakeWeatherData.city}/>
+      <WeatherItem WeatherData={fakeWeatherData.list} />
+    </div>
+  );
+};
 
 export default App;
-
